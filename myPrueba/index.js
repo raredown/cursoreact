1790ded8caff5d7e7c1c76734bffd72e914ad554
raredown <raredown@gmail.com>
2022-03-08 13:11:44 +0100

    practica4

--- myPrueba/index.js
+++ myPrueba/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './todoApp/App';
+import App from './Listing-4.20-ProfileCard-Step6-Completed/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
