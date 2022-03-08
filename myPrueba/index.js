d286caed3e0cd4b51f7b818782c7843f89c650e3
raredown <raredown@gmail.com>
2022-03-08 12:54:19 +0100

    Practica 4 primera parte

--- myPrueba/index.js
+++ myPrueba/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './modulo2/estado4';
+import App from './todoApp/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
